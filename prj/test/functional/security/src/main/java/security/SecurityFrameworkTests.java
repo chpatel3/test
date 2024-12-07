73a17465ad765755159be513792a965aa345fc37
chrish <7pwjtfdzwh@privaterelay.appleid.com>
2024-12-06 20:20:01 -0500

    [2024-12-06] [20:20:01]

--- prj/test/functional/security/src/main/java/security/SecurityFrameworkTests.java
+++ prj/test/functional/security/src/main/java/security/SecurityFrameworkTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2022, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2024, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * https://oss.oracle.com/licenses/upl.
@@ -172,5 +172,5 @@ public class SecurityFrameworkTests
     /**
      * Federated cache config
      */
-    public static final String CACHE_CONFIG = "with-fed-cache-config.xml";
+    public static final String CACHE_CONFIG = "with-dist-cache-config.xml";
     }
